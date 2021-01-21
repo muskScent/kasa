dc74deed743745eccd5891c50fe2d83facb0ecc9
Othmane <othmane.abouelyzza@gmail.com>
2021-01-21 22:25:55 +0100

    Added 404 not found component and route

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./NotFound.css";
 
 export default class NotFound extends Component {
   render() {
-    return <h1>404 NOT FOUND</h1>;
+    return (
+      <div className="text-center not-found">
+        <div className="not-found-404">
+          <span>404</span>
+        </div>
+        <h2>Oups! La page que vous demandez n'existe pas.</h2>
+        <div className="back-home">
+          <Link to="/">Retourner sur la page d'accueil</Link>
+        </div>
+      </div>
+    );
   }
 }
