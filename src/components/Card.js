c09c48a868bffbb5415a91b47ff768b9045beceb
Othmane <othmane.abouelyzza@gmail.com>
2021-02-02 22:56:21 +0100

    Redirect on bad card route (Non existing id)

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
+import RentData from "../data/data.json";
 import Slider from "./Slider";
 
 export default class Card extends Component {
+  componentDidMount() {
+    if (
+      RentData.filter((o) => o.id === this.props.match.params.id).length === 0
+    ) {
+      this.props.history.push("/404");
+    }
+  }
+
   render() {
     return (
       <div className="container">
