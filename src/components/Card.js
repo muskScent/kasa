cd391128ea7da5e1bead210590260aae1bbe0971
Othmane <othmane.abouelyzza@gmail.com>
2021-02-13 23:07:13 +0100

    Added host info

--- src/components/Card.js
+++ src/components/Card.js
@@ -40,9 +40,9 @@ export default class Card extends Component {
   };
 
   render() {
-    const slides = RentData.filter(
-      (o) => o.id === this.props.match.params.id
-    ).map((o) => o.pictures)[0];
+    const cardInfo = RentData.find((o) => o.id === this.props.match.params.id);
+    const slides = cardInfo.pictures;
+    const host = cardInfo.host;
 
     const description = `Vous serez à 50m du canal Saint-martin où vous pourrez pique-niquer l'été et à côté de nombreux bars et restaurants. Au cœur de Paris avec 5 lignes de métro et de nombreux bus. Logement parfait pour les voyageurs en solo et les voyageurs d'affaires. Vous êtes à 1 station de la gare de l'est (7 minutes à pied). `;
 
@@ -57,6 +57,27 @@ export default class Card extends Component {
     return (
       <>
         <Slider slides={slides} />
+        <div className="card--info">
+          <div className="card--info--summary">
+            <h2>Cozy loft on the Canal Saint-Martin</h2>
+            <h3>Paris, Île-de-France</h3>
+            <div className="card--info--summary-chips">
+              <div className="card--chip">Cozy</div>
+              <div className="card--chip">Canal</div>
+              <div className="card--chip">Paris 10</div>
+            </div>
+          </div>
+          <div className="card--info--rating">
+            <div className="card--info--portrait">
+              <p className="card--info__hostname">Alexandre Dumas</p>
+              <img
+                src={host.picture}
+                alt="Host portrait"
+                className="circle--img"
+              />
+            </div>
+          </div>
+        </div>
         <div className="card--details">
           <Accordion
             title="Description"
